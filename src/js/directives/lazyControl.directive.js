d39f781043ac1270abad8cb79f6b174ae44c3b58
Romain Gonord <romain.gonord.opensource@neteyes.org>
2016-04-10 17:21:29 +0800

    add placeholder management for default value into adama-web

--- src/js/directives/lazyControl.directive.js
+++ src/js/directives/lazyControl.directive.js
@@ -5,6 +5,7 @@ angular.module('adama-web').directive('lazyControl', function($rootScope, $filte
 	return {
 		link: function postLink(scope, element, attrs) {
 			var id, additionalLabelAttributes, labelScreenOnly, labelContainer;
+			var isPlaceholderForced = false;
 			if (attrs.type === 'checkbox') {
 				element.wrap('<div class="checkbox"><label></label></div>');
 				element.after('<span></span>');
@@ -16,15 +17,25 @@ angular.module('adama-web').directive('lazyControl', function($rootScope, $filte
 				if (labelScreenOnly) {
 					additionalLabelAttributes += ' class="sr-only"';
 				}
+				if (attrs.placeholder){
+					isPlaceholderForced = true;
+				}
 				element.attr('id', id);
 				element.addClass('form-control');
 				element.wrap('<div class="form-group"></div>');
 				element.before('<label' + additionalLabelAttributes + '></label>');
 				labelContainer = element.prev().eq(0);
 			}
-			labelContainer.html(translateFilter(attrs.lazyControlLabelKey));
+			var initLabelAndPlaceholder = function() {
+				var label = translateFilter(attrs.lazyControlLabelKey);
+				labelContainer.html(label);
+				if (!isPlaceholderForced) {
+					element.attr('placeholder', label);
+				}
+			};
+			initLabelAndPlaceholder();
 			$rootScope.$on('$translateChangeSuccess', function() {
-				labelContainer.html(translateFilter(attrs.lazyControlLabelKey));
+				initLabelAndPlaceholder();
 			});
 		}
 	};
