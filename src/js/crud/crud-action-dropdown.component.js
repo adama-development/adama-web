b9177bf3bdce1a8eee649f6591a71e831adb44bb
Romain Gonord <romain.gonord.opensource@neteyes.org>
2016-05-16 16:37:50 +0800

    transclude on crud-action-dropdown

--- src/js/crud/crud-action-dropdown.component.js
+++ src/js/crud/crud-action-dropdown.component.js
@@ -4,6 +4,7 @@ angular.module('adama-web').component('crudActionDropdown', {
 	templateUrl: /* @ngInject */ function(adamaConstant) {
 		return adamaConstant.adamaWebToolkitTemplateUrl.crudActionDropdown;
 	},
+	transclude: true,
 	bindings: {
 		routeMapping: '<',
 		disableView: '<',
