84d6d2919cf69f7f1ace53185aaf14a178f65465
Romain Gonord <romain.gonord.opensource@neteyes.org>
2016-07-12 17:51:20 +0800

    new build

--- dist/adama-web.js
+++ dist/adama-web.js
@@ -13,6 +13,7 @@ angular.module('adama-web', [ //
 	'ngFileSaver', //
 	'ngFileUpload', //
 	'angular-jwt', //
+	'angular-logger', //
 	'angular-loading-bar' //
 ]);
 
@@ -154,6 +155,14 @@ angular.module('adama-web').config(["$httpProvider", function($httpProvider) {
 	$httpProvider.interceptors.push('authExpiredInterceptor');
 	$httpProvider.interceptors.push('authInterceptor');
 	$httpProvider.interceptors.push('notificationInterceptor');
+}]);
+
+angular.module('adama-web').config(["logEnhancerProvider", function(logEnhancerProvider) {
+	logEnhancerProvider.prefixPattern = '%s::[%s]>';
+	logEnhancerProvider.datetimePattern = 'DD/MM/YYYY HH:mm:ss';
+	logEnhancerProvider.logLevels = {
+		'*': logEnhancerProvider.LEVEL.OFF
+	};
 }]);
 
 'use strict';
@@ -567,7 +576,6 @@ angular.module('adama-web').controller('ResetPasswordCtrl', ["$location", "$stat
 			key: $location.search().key,
 			newPassword: newPassword
 		}).then(function() {
-			console.log('everything is awesome');
 			var origin = $location.search().origin;
 			console.log('origin', origin);
 			if (origin === 'front') {
@@ -1262,7 +1270,7 @@ angular.module('adama-web').directive('hasAuthority', ["Principal", function(Pri
 /*jscs:disable requireDotNotation*/
 'use strict';
 
-angular.module('adama-web').factory('authExpiredInterceptor', ["$injector", "$q", "adamaConstant", function($injector, $q, adamaConstant) {
+angular.module('adama-web').factory('authExpiredInterceptor', ["$injector", "$q", "$log", "adamaConstant", function($injector, $q, $log, adamaConstant) {
 	var getHttpService = (function() {
 		var service;
 		return function() {
@@ -1279,11 +1287,12 @@ angular.module('adama-web').factory('authExpiredInterceptor', ["$injector", "$q"
 
 	return {
 		responseError: function(response) {
+			var log = $log.getInstance('adama-web.interceptors');
 			var config = response.config;
 			if (response.status === 401 && config && config.url.indexOf(adamaConstant.apiBase) === 0 && config.url.indexOf(adamaConstant.apiBase + 'login/authenticate') !== 0) {
-				console.log('authExpiredInterceptor error 401, refresh token');
+				log.debug('authExpiredInterceptor error 401, refresh token');
 				return getAdamaTokenService().refreshAndGetToken().then(function() {
-					console.log('authExpiredInterceptor token is refresh, reset Authorization header');
+					log.debug('authExpiredInterceptor token is refresh, reset Authorization header');
 					config.headers['Authorization'] = undefined;
 					return getHttpService()(config);
 				});
@@ -1297,7 +1306,7 @@ angular.module('adama-web').factory('authExpiredInterceptor', ["$injector", "$q"
 /*jscs:disable requireDotNotation*/
 'use strict';
 
-angular.module('adama-web').factory('authInterceptor', ["$injector", "adamaConstant", function($injector, adamaConstant) {
+angular.module('adama-web').factory('authInterceptor', ["$injector", "$log", "adamaConstant", function($injector, $log, adamaConstant) {
 	var getAdamaTokenService = (function() {
 		var service;
 		return function() {
@@ -1308,11 +1317,12 @@ angular.module('adama-web').factory('authInterceptor', ["$injector", "adamaConst
 	return {
 		// Add authorization token to headers
 		request: function(config) {
+			var log = $log.getInstance('adama-web.interceptors');
 			config.headers = config.headers || {};
 			if (!config.headers['Authorization'] && config.url.indexOf(adamaConstant.apiBase) === 0) {
-				console.log('authInterceptor need authorization, getting token');
+				log.debug('authInterceptor need authorization, getting token');
 				return getAdamaTokenService().getToken().then(function(token) {
-					console.log('add token to http config');
+					log.debug('add token to http config');
 					if (token) {
 						config.headers['Authorization'] = 'Bearer ' + token;
 					}
@@ -1415,7 +1425,7 @@ angular.module('adama-web').factory('adamaResourceConfig', ["ParseLinks", "binar
 			headers: {
 				'Accept': 'application/vnd.ms-excel'
 			},
-			transformResponse: binaryService.transformResponseToPdf
+			transformResponse: binaryService.transformResponseToExcel
 		},
 		'massImportXls': {
 			method: 'POST',
@@ -1439,7 +1449,9 @@ angular.module('adama-web').factory('adamaResourceConfig', ["ParseLinks", "binar
 /*jscs:disable requireDotNotation*/
 'use strict';
 
-angular.module('adama-web').factory('adamaTokenService', ["$rootScope", "$http", "$q", "$state", "jwtHelper", "localStorageService", "adamaConstant", function($rootScope, $http, $q, $state, jwtHelper, localStorageService, adamaConstant) {
+angular.module('adama-web').factory('adamaTokenService', ["$rootScope", "$http", "$q", "$state", "$log", "jwtHelper", "localStorageService", "adamaConstant", function($rootScope, $http, $q, $state, $log, jwtHelper, localStorageService, adamaConstant) {
+	var log = $log.getInstance('adama-web.service');
+
 	var api = {};
 
 	var getPayload = function(key) {
@@ -1470,7 +1482,7 @@ angular.module('adama-web').factory('adamaTokenService', ["$rootScope", "$http",
 	api.getToken = function() {
 		var token = localStorageService.get('token');
 		if (token && jwtHelper.isTokenExpired(token)) {
-			console.log('adamaTokenService.getToken token is expired');
+			log.debug('adamaTokenService.getToken token is expired');
 			return api.refreshAndGetToken();
 		}
 		return $q.when(token);
@@ -1734,10 +1746,10 @@ angular.module('adama-web').factory('binaryFileService', ["$http", "$q", "Upload
 angular.module('adama-web').factory('binaryService', ["FileSaver", function(FileSaver) {
 	var api = {};
 
-	api.transformResponseToPdf = function(data, headersGetter, status) {
+	var saveFile = function(data, headersGetter, status, contentType, defaultFileName) {
 		if (status === 200) {
 			var dataBlob = new Blob([data], {
-				type: 'application/pdf'
+				type: contentType
 			});
 			var contentDisposition = headersGetter('Content-Disposition');
 			var filename;
@@ -1745,10 +1757,19 @@ angular.module('adama-web').factory('binaryService', ["FileSaver", function(File
 				filename = contentDisposition.substring('attachment;filename = '.length);
 			}
 			if (!filename) {
-				filename = 'file.pdf';
+				filename = defaultFileName;
 			}
 			FileSaver.saveAs(dataBlob, filename);
 		}
+		return data;
+	};
+
+	api.transformResponseToPdf = function(data, headersGetter, status) {
+		return saveFile(data, headersGetter, status, 'application/pdf', 'file.pdf');
+	};
+
+	api.transformResponseToExcel = function(data, headersGetter, status) {
+		return saveFile(data, headersGetter, status, 'application/vnd.ms-excel', 'file.xls');
 	};
 
 	return api;
@@ -1835,7 +1856,9 @@ angular.module('adama-web').service('ParseLinks', function() {
 
 'use strict';
 
-angular.module('adama-web').factory('Principal', ["$http", "$q", "$rootScope", "$resource", "$state", "adamaConstant", "adamaTokenService", function($http, $q, $rootScope, $resource, $state, adamaConstant, adamaTokenService) {
+angular.module('adama-web').factory('Principal', ["$http", "$q", "$rootScope", "$resource", "$state", "$log", "adamaConstant", "adamaTokenService", function($http, $q, $rootScope, $resource, $state, $log, adamaConstant, adamaTokenService) {
+	var log = $log.getInstance('adama-web.service');
+
 	var Password = $resource(adamaConstant.apiBase + 'account/change_password');
 	var PasswordResetInit = $resource(adamaConstant.apiBase + 'account/reset_password/init', {}, {});
 	var PasswordResetFinish = $resource(adamaConstant.apiBase + 'account/reset_password/finish');
@@ -1931,7 +1954,7 @@ angular.module('adama-web').factory('Principal', ["$http", "$q", "$rootScope", "
 			var isAuthenticated = api.isAuthenticated();
 			// an authenticated user can't access to login pages
 			if (isAuthenticated && $rootScope.toState.name && $rootScope.toState.name === 'auth.signin') {
-				console.log('redirect to main as user is authenticated and is trying to access signin');
+				log.debug('redirect to main as user is authenticated and is trying to access signin');
 				return $state.go('app.main', {}, {
 					location: 'replace'
 				});
@@ -1939,7 +1962,7 @@ angular.module('adama-web').factory('Principal', ["$http", "$q", "$rootScope", "
 			if ((!$rootScope.toState.data || !$rootScope.toState.data.authorities) && !isAuthenticated) {
 				// user is not signed in but desired state needs an
 				// authenticated user
-				console.log('redirect to signin as user is not authenticated and page is restricted (default conf with no authorities on state)');
+				log.debug('redirect to signin as user is not authenticated and page is restricted (default conf with no authorities on state)');
 				return $state.go('auth.signin', {}, {
 					location: 'replace'
 				});
@@ -1952,7 +1975,7 @@ angular.module('adama-web').factory('Principal', ["$http", "$q", "$rootScope", "
 				if (isAuthenticated) {
 					// user is signed in but not authorized for
 					// desired state
-					console.log('redirect to accessDenied as user is authenticated and does not have right privileges');
+					log.debug('redirect to accessDenied as user is authenticated and does not have right privileges');
 					return $state.go('auth.accessDenied', {}, {
 						location: 'replace'
 					});
@@ -1965,7 +1988,7 @@ angular.module('adama-web').factory('Principal', ["$http", "$q", "$rootScope", "
 				$rootScope.previousStateNameParams = $rootScope.toStateParams;
 				// now, send them to the signin state so they
 				// can log in
-				console.log('redirect to signin as user is not authenticated and page is restricted (conf with explicit authorities on state)');
+				log.debug('redirect to signin as user is not authenticated and page is restricted (conf with explicit authorities on state)');
 				return $state.go('auth.signin', {}, {
 					location: 'replace'
 				});
